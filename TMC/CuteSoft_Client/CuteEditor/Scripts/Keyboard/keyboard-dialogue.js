933cefb4722b6f9fc18ee80bdb098fa48a985250
tmccord <localcord@gmail.com>
2015-08-14 08:10:59 +0530

    folder name changed to TMC.Web

--- TMC/CuteSoft_Client/CuteEditor/Scripts/Keyboard/keyboard-dialogue.js
+++
@@ -1,1 +0,0 @@
-var OxOffec=["value","keyboard_area"];function afficher(Ox282){document.getElementById(OxOffec[1])[OxOffec[0]]=Ox282;} ;function rechercher(){return document.getElementById(OxOffec[1])[OxOffec[0]];} ;
